d2f658b37ba16ffc62c4d0416fbe93f21c21ace5
Soulsplitter <ryonbulens@outlook.com>
2021-11-24 12:13:28 +0100

    Updates on the new creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,26 +11,48 @@ export class AppComponent implements OnInit {
   title: string;
   cities: City[];
   toggleMsg: string = 'Verberg lijst met steden.';
+  newCity: string = '';
+  textVisible:boolean = true;
+  currentCity:City;
+  cityPhoto:string = '';
 
-    ngOnInit() {
-  this.title = 'Mijn favoriete steden via Model'
-  this.cities = [
-    new City(1, 'Groningen', 'GR'),
-    new City(2, 'Hengelo', 'O'),
-    new City(3, 'Den Haag', 'ZH'),
-    new City(4, 'Enschede', 'O'),
+  ngOnInit() {
+    this.title = 'Mijn favoriete steden via Model'
+    this.cities = [
+      new City(1, 'Groningen', 'GR'),
+      new City(2, 'Hengelo', 'O'),
+      new City(3, 'Den Haag', 'ZH'),
+      new City(4, 'Enschede', 'O'),
 
-  ];
-  this.showCities = this.cities.length > 3;
-}
-btnClick(){
-  alert('Uw bestelling wordt uitgevoerd!')
-}
-toggleCities(){
-  this.showCities = !this.showCities;
-  this.showCities
-    ? this.toggleMsg = 'verberg lijst met steden.'
-    : this.toggleMsg = 'Toon de lijst met steden.';
-}
+    ];
+    this.showCities = this.cities.length > 3;
 
+  }
+  btnClick() {
+    alert('Uw bestelling wordt uitgevoerd!')
+  }
+  toggleCities() {
+    this.showCities = !this.showCities;
+    this.showCities
+      ? this.toggleMsg = 'verberg lijst met steden.'
+      : this.toggleMsg = 'Toon de lijst met steden.';
+  }
+  showCity(city: City) {
+    this.currentCity = city;
+    this.cityPhoto = `assets/img/${this.currentCity.name}.jpg`;
+    alert('Uw favoriete stad is: ' + city.name);
+  }
+  changeCity(value: string) {
+    this.newCity = value;
+  }
+  addCity(value:string){
+    let addedCity = new City(
+      this.cities.length +1, // id
+      value,        //naam
+      'Onbekend');  //provincie
+      this.cities.push(addedCity);
+  }
+  toggleText() {
+    this.textVisible = !this.textVisible;
+  }
 }
